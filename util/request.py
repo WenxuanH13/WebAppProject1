7349c728f4b17d2882ddb27a9ef8866194f37f3f
ToNewAutumn <wenxuanhuang5@gmail.com>
2024-02-12 09:33:53 -0500

    cookie header still has yet to be completed

--- util/request.py
+++ util/request.py
@@ -23,10 +23,16 @@ class Request:
             if line == '':
                 break
             part = line.split(':', 1)
-            key = part[0]
+            key = part[0].strip()
             value = part[1].strip()
             self.headers[key] = value
-
+        
+        bodyIndex = decodeString.find('\r\n\r\n')
+        if bodyIndex != -1:
+            self.body = request[bodyIndex + len('\r\n\r\n'):]
+        
+        
+        
 
 def test1():
     request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
@@ -40,6 +46,29 @@ def test1():
     # It's recommended that you complete this test and add others, including at least one
     # test using a POST request. Also, ensure that the types of all values are correct
 
+def test2():
+    request = Request(b'POST / HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nhello')
+    assert request.method == "POST"
+    assert "Content-Type" in request.headers
+    assert request.headers["Content-Type"] == "text/plain"  # note: The leading space in the header value must be removed
+    assert "Content-Length" in request.headers
+    assert request.headers["Content-Length"] == "5" # note: The leading space in the header value must be removed
+    assert request.body == b"hello"
+
+def test3():
+    request = Request(b'POST /test HTTP/1.1\r\nHost: foo.example\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 27\r\n\r\nfield1=value1&field2=value2')
+    assert request.method == "POST"
+    assert request.path == "/test"
+    assert request.http_version == "HTTP/1.1"
+    assert "Host" in request.headers
+    assert request.headers["Host"] == "foo.example"  # note: The leading space in the header value must be removed
+    assert "Content-Type" in request.headers
+    assert request.headers["Content-Type"] == "application/x-www-form-urlencoded"  # note: The leading space in the header value must be removed
+    assert "Content-Length" in request.headers
+    assert request.headers["Content-Length"] == "27" # note: The leading space in the header value must be removed
+    assert request.body == b"field1=value1&field2=value2"
 
 if __name__ == '__main__':
     test1()
+    test2()
+    test3()
